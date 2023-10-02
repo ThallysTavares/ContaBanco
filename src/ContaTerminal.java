ce15df267ceb6ac7ad8e7d7e1a6c00a35fc341c9
ThallysTavares <thallyschok@gmail.com>
2023-10-02 17:09:28 -0300

    modified:   bin/ContaTerminal.class modified:   src/ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo ");
+
+        Scanner input = new Scanner(System.in);
+
+    int numero;
+    int agencia;
+    String nome;
+    String sobreNome;
+    double saldo;
+    
+    System.out.println("Digite seu nome: ");
+    nome = input.nextLine();
+
+    System.out.println("Digite seu sobrenome: ");
+    sobreNome = input.nextLine();
+
+    System.out.println("Digite seu numero da conta: ");
+    numero = input.nextInt();
+
+    System.out.println("Digite sua agência: ");
+    agencia = input.nextInt();
+
+    System.out.println("Digite seu saldo: ");
+    saldo = input.nextDouble();
+    
+    System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " +  saldo + " já está disponível para saque.");
+
+    input.close();
     }
 }
